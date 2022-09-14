8592474d77ea84a58bd5aa32410272f3e8c99ac8
tm9704 <tm9704@korea.ac.kr>
2022-09-14 22:12:14 +0900

    Drawing App: Ready of BrushSizeSelector

--- app/src/main/java/com/tm9704/drawingapp/DrawingView.kt
+++ app/src/main/java/com/tm9704/drawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.tm9704.drawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -29,7 +30,7 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        /*mBrushSize = 20.toFloat()*/
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int){
@@ -91,7 +92,13 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
         return true
     }
 
+    fun setSizeForBrush(newSize: Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+            newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
+    }
 
+    // An inner class for custom path with two params as color and stroke
     internal inner class CustomPath(var color: Int,
                                     var brushThickness: Float) : Path(){
 
