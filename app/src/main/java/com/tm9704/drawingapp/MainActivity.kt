dc39155e805f7a5736991e030a38d87e9dc99e28
tm9704 <tm9704@korea.ac.kr>
2022-09-20 20:43:54 +0900

    Drawing App: Implement Custom Progress Bar & Hide

--- app/src/main/java/com/tm9704/drawingapp/MainActivity.kt
+++ app/src/main/java/com/tm9704/drawingapp/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+    var customProgressDialog : Dialog? = null
 
     val openGalleryLauncher: ActivityResultLauncher<Intent> =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
@@ -96,6 +97,7 @@ class MainActivity : AppCompatActivity() {
         val ibSave : ImageButton = findViewById(R.id.ib_save)
         ibSave.setOnClickListener{
             if(isReadStorageAllowed()){
+                showProgressDialog()
                 lifecycleScope.launch{
                     val flDrawingView:FrameLayout = findViewById(R.id.fl_drawing_view_container)
 
@@ -223,6 +225,7 @@ class MainActivity : AppCompatActivity() {
                     result = f.absolutePath
 
                     runOnUiThread{
+                        cancelProgressDialog()
                         if(result.isNotEmpty()){
                             Toast.makeText(
                                 this@MainActivity,
@@ -245,5 +248,20 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return result
+    }
+
+    private fun showProgressDialog() {
+        customProgressDialog = Dialog(this@MainActivity)
+
+        customProgressDialog?.setContentView(R.layout.dialog_custom_progress)
+
+        customProgressDialog?.show()
+    }
+
+    private fun cancelProgressDialog(){
+        if(customProgressDialog != null){
+            customProgressDialog?.dismiss()
+            customProgressDialog = null
+        }
     }
 }
