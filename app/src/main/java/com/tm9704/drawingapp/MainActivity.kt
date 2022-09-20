532d9728f7b85b0ea80e18740f68878b30c0ec27
tm9704 <tm9704@korea.ac.kr>
2022-09-20 20:54:01 +0900

    Drawing App: Final Touch & Add Shared Image Function

--- app/src/main/java/com/tm9704/drawingapp/MainActivity.kt
+++ app/src/main/java/com/tm9704/drawingapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.media.MediaScannerConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -232,6 +233,7 @@ class MainActivity : AppCompatActivity() {
                                 "File saved successfully: $result",
                                 Toast.LENGTH_SHORT
                             ).show()
+                            shareImage(result)
                         }else{
                             Toast.makeText(
                                 this@MainActivity,
@@ -262,6 +264,18 @@ class MainActivity : AppCompatActivity() {
         if(customProgressDialog != null){
             customProgressDialog?.dismiss()
             customProgressDialog = null
+        }
+    }
+
+    private fun shareImage(result: String){
+        MediaScannerConnection.scanFile(this, arrayOf(result), null){
+            path, uri ->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+            startActivity(Intent.createChooser(shareIntent, "Share"))
+
         }
     }
 }
